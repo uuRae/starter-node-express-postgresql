7ca0220a97d136c97f0283366b69618ce1e18eb8
Katrina Pingryn <katep@indeed.com>
2024-02-16 19:48:33 -0600

    joins with knex

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,11 +1,28 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
+
+const addCategory = mapProperties({
+  category_id: "category.category_id",
+  category_name: "category.category_name",
+  category_description: "category.category_description",
+});
 
 function list() {
   return knex("products").select("*");
 }
 
-function read(productId) {
-  return knex("products").select("*").where({ product_id: productId }).first();
+// function read(productId) {
+//   return knex("products").select("*").where({ product_id: productId }).first();
+// }
+
+function read(product_id) {
+  return knex("products as p")
+    .join("products_categories as pc", "p.product_id", "pc.product_id")
+    .join("categories as c", "pc.category_id", "c.category_id")
+    .select("p.*", "c.*")
+    .where({ "p.product_id": product_id })
+    .first()
+    .then(addCategory);
 }
 
 function listOutOfStockCount() {
